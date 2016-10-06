124df58e9cf35b7cb91ad848eac2057c520b4af3
Bryan Smith <bsmith@learningobjects.com>
2016-10-06 19:30:42 -0400

    update-sprint-data: auth w/ Jira and fetch things. Uses property file for host.

--- update-sprint-data/build.sbt
+++ update-sprint-data/build.sbt
@@ -2,4 +2,10 @@ name := "update-sprint-data"
 version := "1.0"
 scalaVersion := "2.11.8"
 
+libraryDependencies ++= Seq(
+  "org.scalaj" %% "scalaj-http" % "2.3.0"
+)
+
+resolvers += Resolver.sonatypeRepo("public")
+
 mainClass in Compile := Some("com.learningobjects.tai.Main")
--- update-sprint-data/src/main/scala/com/learningobjects/tai/Main.scala
+++ update-sprint-data/src/main/scala/com/learningobjects/tai/Main.scala
@@ -1,19 +1,38 @@
 package com.learningobjects.tai
 
+import java.io.FileInputStream
+import java.net.HttpCookie
+import java.util.Properties
+
+import scalaj.http._
+
 object Main {
 
   /**
-    *             (^-^*)/
+    *     (^-^*)/
     */
   def main(args: Array[String]): Unit = {
 
     val (username, password) = readCredentials
 
+    val cookies = Jira.authenticate(username, password)
 
+    val request = Http(Urls.fromPath("/rest/api/2/issue/ABC-6782")).cookies(cookies)
+
+    println(s"DEBUG: ${request.asString}")
   }
 
   /**
-    * Ask user for credentials from stdin.
+    *     (っ˘ڡ˘ς)
+    */
+  lazy val properties = {
+    val prop = new Properties()
+    prop.load(new FileInputStream("run.properties"))
+    prop
+  }
+
+  /**
+    *     ( ˘▽˘)っ♨
     */
   def readCredentials = {
     print("Username: ")
@@ -23,5 +42,40 @@ object Main {
     val password = System.console().readPassword()
 
     (username, new String(password))
+  }
+
+  /**
+    *     (↼_↼)
+    */
+  object Jira {
+
+    def authenticate(username:String, password: String): IndexedSeq[HttpCookie] = {
+
+      val payload = s"""
+                       | {
+                       |   "username": "${username}",
+                       |   "password": "${password}"
+                       | }
+       """.stripMargin
+
+      val request = Http(Urls.Auth).header("content-type", "application/json")
+        .postData(payload)
+
+      val response = request.asString
+
+      response.cookies
+    }
+  }
+
+  /**
+    *     (⇀_⇀)
+    */
+  object Urls {
+
+    private lazy val host = properties.getProperty("jira.host")
+
+    def fromPath(path:String)  = s"$host$path"
+
+    val Auth = fromPath("/rest/auth/1/session")
   }
 }
