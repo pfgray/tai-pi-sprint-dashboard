c256ef7e539e49a4e9bf5d575e4159977125053f
Bryan Smith <bsmith@learningobjects.com>
2016-10-06 18:23:17 -0400

    update-sprint-data: read credentials

--- update-sprint-data/src/main/scala/com/learningobjects/tai/Main.scala
+++ update-sprint-data/src/main/scala/com/learningobjects/tai/Main.scala
@@ -2,7 +2,26 @@ package com.learningobjects.tai
 
 object Main {
 
+  /**
+    *             (^-^*)/
+    */
   def main(args: Array[String]): Unit = {
-    println("Yo...")
+
+    val (username, password) = readCredentials
+
+
+  }
+
+  /**
+    * Ask user for credentials from stdin.
+    */
+  def readCredentials = {
+    print("Username: ")
+    val username = Console.in.readLine()
+
+    print("Password: ")
+    val password = System.console().readPassword()
+
+    (username, new String(password))
   }
 }
