609949c311b3150572a6b96bb7584d473997ce56
Gustavo Torrico <gtorrico@learningobjects.com>
2016-10-06 19:56:47 -0400

    Adding shell for charts

--- tai-sprint-dashboard-webapp/src/main/scala/com/learningobjects/tai/app/TaiScalatraServlet.scala
+++ tai-sprint-dashboard-webapp/src/main/scala/com/learningobjects/tai/app/TaiScalatraServlet.scala
@@ -1,16 +1,10 @@
 package com.learningobjects.tai.app
 
-import org.scalatra._
-
 class TaiScalatraServlet extends TaiSprintDashboardWebappStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType = "text/html"
+    layoutTemplate("index.jade", ("pageTitle", "TAI Burndown"))
   }
 
 }
