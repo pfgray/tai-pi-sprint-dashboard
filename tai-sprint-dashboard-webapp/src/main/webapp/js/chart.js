dd4017b414d1e81ec2127a10677b6ead9dc0c231
Gustavo Torrico <gtorrico@learningobjects.com>
2016-10-06 21:00:40 -0400

    Chart rendering for sample data

--- tai-sprint-dashboard-webapp/src/main/webapp/js/chart.js
+++ tai-sprint-dashboard-webapp/src/main/webapp/js/chart.js
@@ -3,94 +3,41 @@ req.open('GET', '/js/samples/current-sprint.json', true);
 req.send(null);
 
 const getTitle = function(data) {
-    return data.sprint + ' -- completed:' + data.completed + ', remaining: ' + data.remaining;
+    return data.sprint + ' -- ' +  data.days_remaining + ' day(s) remaining';
+};
+
+const getDayLabels = function(data) {
+    const days = _.map(data.history, function(h) { return new Date(h.date) });
+    var lastDay = _.last(days);
+    for (var i = 0; i < data.days_remaining; i++) {
+        var inc = (lastDay.getDay() != 5) ? 1 : 3;
+        const nextDay = new Date();
+        nextDay.setDate(lastDay.getDate() + inc);
+        days.push(nextDay);
+        lastDay = nextDay;
+    }
+    return _.map(days, function(d) { return d.toLocaleString('en-US', { weekday: 'short' })});
 };
 
 const renderChart = function(data) {
     const chart = new Chart(document.getElementById('burndown-chart'),
         {
-            type: 'bar',
+            label: 'test',
+            type: 'line',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: getDayLabels(data),
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
+                    label: 'Remaining: ' + data.remaining,
+                    data: _.map(data.history, function(h) { return h.remaining; })
+                },{
+                    label: 'Completed: ' + data.completed,
+                    data: _.map(data.history, function(h) { return h.completed; })
                 }]
             },
             options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
+                title: {display: true, text: getTitle(data), fontSize: 24}
             }
         }
-        /*
-        {
-        //label: getTitle(data),
-        data:{
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fill: false,
-                    lineTension: 0.1,
-                    backgroundColor: "rgba(75,192,192,0.4)",
-                    borderColor: "rgba(75,192,192,1)",
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: "rgba(75,192,192,1)",
-                    pointBackgroundColor: "#fff",
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                    pointHoverBorderColor: "rgba(220,220,220,1)",
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    spanGaps: false,
-                }
-            ]
-        data: {
-            labels: ['Wed', 'Thur', 'Fri', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Mon', 'Tue'],
-            datasets: [
-                {
-                    type: 'line',
-                    label: 'remaining',
-                    data: []
-                },
-                {
-                    type: 'line',
-                    label: 'completed',
-                    data: []
-                }
-
-            ]
-        }
-    }
-         */
     );
 };
 
