52182b09e35bed86887ff7957b6635e3b77e194d
Paul Gray <pgray@learningobjects.com>
2016-10-06 20:44:35 -0400

    Adding speak

--- tai-sprint-dashboard-webapp/src/main/scala/com/learningobjects/tai/app/TaiScalatraServlet.scala
+++ tai-sprint-dashboard-webapp/src/main/scala/com/learningobjects/tai/app/TaiScalatraServlet.scala
@@ -1,10 +1,22 @@
 package com.learningobjects.tai.app
 
+import sys.process._
+
 class TaiScalatraServlet extends TaiSprintDashboardWebappStack {
 
   get("/") {
     contentType = "text/html"
     layoutTemplate("index.jade", ("pageTitle", "TAI Burndown"))
+  }
+
+  get("/speak") {
+    contentType = "text/html"
+    layoutTemplate("speak.jade", ("pageTitle", "Speak"))
+  }
+
+  post("/speakit") {
+    val result = s"echo '${params.get("text")}' | festival --tts" !
+    redirect("/speak")
   }
 
 }
