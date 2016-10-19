37170a0fe906b6d89d2636dba955f1e9a9993b8d
Bryan Smith <bsmith@learningobjects.com>
2016-10-19 19:05:13 -0400

    Filter out stories completed before sprint started

--- update-sprint-data/build.sbt
+++ update-sprint-data/build.sbt
@@ -5,7 +5,8 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.scalaj" %% "scalaj-http" % "2.3.0",
   "com.typesafe.play" %% "play-json" % "2.5.0",
-  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+  "joda-time" %% "joda-time" %% "2.9.4"
 )
 
 resolvers += Resolver.sonatypeRepo("public")
--- update-sprint-data/src/main/scala/com/learningobjects/tai/Main.scala
+++ update-sprint-data/src/main/scala/com/learningobjects/tai/Main.scala
@@ -1,6 +1,10 @@
 package com.learningobjects.tai
 
 import java.net.HttpCookie
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+import org.joda.time.DateTime
 
 object Main {
 
@@ -14,17 +18,24 @@ object Main {
     implicit val cookies: IndexedSeq[HttpCookie] = Jira.authenticate(username, password)
 
     Jira.activeSprint.foreach(sprint => {
+
+      // Current sprint start date
+      val format = new SimpleDateFormat("dd/MMM/yy hh:mm a")
+      val sprintStart = format.parse(sprint.startDate)
+
+      // Filter out anything from before this sprint
       val separateIssues =
         Jira.issueSummaries(sprint.id)
           .map(summary => Jira.issue(summary.key))
+          .filterNot(_.resolutionDate
+            .map(DateTime.parse)
+            .exists(_.toDate.before(sprintStart)))
 
+      // Bind parent tickets to children
       val issues = JiraUtils.bind(separateIssues)
 
       val total = issues.map(JiraUtils.points).sum
-
       val completed = issues.map(JiraUtils.completedPoints).sum
-
-      println(s"DEBUG: ${separateIssues.size} -> ${issues.size}")
 
       println(s"DEBUG: finished ${completed} out of ${total}")
 
@@ -34,7 +45,7 @@ object Main {
         //   1. Create graph using subtasks, parent fields
         //   2. Generate JSON using done, resolutionDate, points
         //
-        println(s"DEBUG: $issue")
+//        println(s"DEBUG: $issue")
       })
 
     })
